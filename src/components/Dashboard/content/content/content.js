a172b2e6847b2e6023439e52d4eae1ac12f10869
KaiYi.C <oschangkai@gmail.com>
2017-11-03 10:21:43 +0800

    Bigger font-size on content

--- src/components/Dashboard/content/content/content.js
+++ src/components/Dashboard/content/content/content.js
@@ -5,7 +5,14 @@ export default class AdminContent extends React.Component {
     return (
       <div>
         <h1>哈囉！</h1>
-        <p style={{ color: "black", paddingLeft: 15, paddingTop: 10 }}>
+        <p
+          style={{
+            color: "black",
+            paddingLeft: 15,
+            paddingTop: 10,
+            fontSize: 14
+          }}
+        >
           這裡是 Triptao 的後台，請在左側選擇你要的欄位，填寫你要的資料唷！
         </p>
       </div>
